42dd0167cdaeb51ac1872c9a2d074b5f785e1aab
Gleyson Sampaio <gleyson@digytal.com.br>
2023-04-18 16:06:52 -0300

    upload e apresentação de imagens

--- src/main/java/com/br/eletronicapp/controllers/produtoController.java
+++ src/main/java/com/br/eletronicapp/controllers/produtoController.java
@@ -8,9 +8,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.br.eletronicapp.models.ProdutoModel;
 import com.br.eletronicapp.services.ProdutoService;
@@ -20,8 +18,7 @@ import java.nio.file.Paths;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/produtos")
@@ -39,6 +36,30 @@ public class produtoController {
     public ResponseEntity<ProdutoModel> insert(@RequestBody ProdutoModel eletronic) {
         service.insertProduto(eletronic);
         return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping(path ="/upload-image/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<ProdutoModel> upload(@PathVariable("id") Integer id, @RequestPart("file") MultipartFile uploadFile) {
+        service.updatePicture(id, uploadFile);
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping("/image/{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable("id") Integer id) {
+
+        try {
+            ProdutoModel produtoModel = service.findProdutoById(id);
+
+            byte[] image = Files.readAllBytes(Paths.get(produtoModel.getImagem()));
+
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(image);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
     
 }
--- src/main/java/com/br/eletronicapp/services/ProdutoService.java
+++ src/main/java/com/br/eletronicapp/services/ProdutoService.java
@@ -4,13 +4,16 @@ package com.br.eletronicapp.services;
  *
  * @author Evandro
  */
+import java.io.File;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.br.eletronicapp.models.ProdutoModel;
 import com.br.eletronicapp.repositories.ProdutoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @AllArgsConstructor
@@ -37,11 +40,38 @@ public class ProdutoService {
         eletronic.setNome(model.getNome());
         eletronic.setDescricao(model.getDescricao());
         eletronic.setFormaDepagamento(model.getFormaDepagamento());
-        eletronic.setImagem(model.getImagem());
+        //eletronic.setImagem(model.getImagem());
         return repository.save(eletronic);
     }
 
     public void deleteProdutoById(Integer id) {
         repository.deleteById(id);
     }
+
+    @Autowired
+    private FileUploadService  fileUploadService;
+    public void updatePicture( Integer id, MultipartFile webFile) {
+
+        try {
+            //ver -> application.properties -> file.upload.folder
+            File fileDisk = fileUploadService.saveOnDisk(webFile);
+
+            String filePath = fileDisk.getAbsolutePath();
+
+            System.out.println(filePath);
+
+            ProdutoModel model = repository.findById(id).get();
+            model.setImagem(filePath);
+
+
+            repository.save(model);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+
 }
